afb14715999177b13272a9891e7c76b281490299
SophiaRenats <90347782+SophiaRenats@users.noreply.github.com>
2021-11-29 14:19:25 -0500

    v0.2
    installed the windowsurface display program

--- myFirstPyGame.py
+++ myFirstPyGame.py
@@ -1,5 +1,10 @@
-# My First PyGame, Sophia Rentas, 11/29/21 2:10, v0.1
+# My First PyGame, Sophia Rentas, 11/29/21 2:17, v0.2
 
 import pygame, sys
 from pygame.locals import *
 
+# Start PyGame
+pygame.init()
+
+# Setup our window. 1
+windowSurface = pygame.display.set_mode((500, 400),0, 32)
