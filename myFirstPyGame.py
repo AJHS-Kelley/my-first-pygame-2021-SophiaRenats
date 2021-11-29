84618a462563acf78b207a9f3926abc9b4865904
SophiaRenats <90347782+SophiaRenats@users.noreply.github.com>
2021-11-29 14:26:30 -0500

    v0.3
    showed what makes the colors(RGB)

--- myFirstPyGame.py
+++ myFirstPyGame.py
@@ -1,4 +1,4 @@
-# My First PyGame, Sophia Rentas, 11/29/21 2:17, v0.2
+# My First PyGame, Sophia Rentas, 11/29/21 2:25, v0.3
 
 import pygame, sys
 from pygame.locals import *
@@ -8,3 +8,12 @@ pygame.init()
 
 # Setup our window. 1
 windowSurface = pygame.display.set_mode((500, 400),0, 32)
+pygame.display.set_caption('Hello, world')
+
+# Setup Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE= (0, 0, 255)
+
