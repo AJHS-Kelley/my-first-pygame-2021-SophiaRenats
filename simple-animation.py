0af546430000f55942e6f16f761a1d2b867c2bcd
SophiaRenats <90347782+SophiaRenats@users.noreply.github.com>
2022-01-05 14:12:13 -0500

    v0.1
    Imported modules for PyGame, System, and Time.
    
    Initialized the Pygame Module.
    
    Setup window dimensions.

--- simple-animation.py
+++ simple-animation.py
@@ -1,1 +1,13 @@
-# Simple Animation with PyGame, Sophia Rentas, 12/08/21, 2:45pm, v0.0
+# Simple Animation with PyGame, Sophia Rentas, 1/5/22, 2:07, v0.1
+
+import pygame, sys, time
+from pygame.locals import *
+
+# Setup PyGame 
+pygame.init()
+
+# Setup the Window
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 400
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+pygame.display.set_caption('Animation Example!')
