a5005ca4dc6164c41d4904e8e3af1c4629c8cbc6
SophiaRenats <90347782+SophiaRenats@users.noreply.github.com>
2022-01-11 13:53:34 -0500

    v0.7
    Created the code to check for boxes leaving window.

--- simple-animation.py
+++ simple-animation.py
@@ -1,4 +1,4 @@
-# Simple Animation with PyGame, Sophia Rentas, 1/11/22, 11:21am, v0.6
+# Simple Animation with PyGame, Sophia Rentas, 1/11/22, 1:49pm, v0.7
 
 from PyGamePractice import GREEN
 import pygame, sys, time
@@ -58,4 +58,29 @@ while True:
                 b['rect'].left += MOVESPEED
                 b['rect'].top -= MOVESPEED 
 
+        if b['rect'].top < 0:
+            # The box has moved past the top.
+            if b['dir'] == UPLEFT:
+                b['dir'] = DOWNLEFT
+            if b['dir'] == UPRIGHT:
+                b['dir'] = DOWNRIGHT
+            if b['rect'].bottom > WINDOWHEIGHT:
+                # The box has moved past the bottom.
+            if b['dir'] == DOWNLEFT:
+                b['dir'] = UPLEFT
+            if b['dir'] == DOWNRIGHT:
+                b['dir'] = UPRIGHT
+            if b['rect'].left <0:
+                #The box has moved past the left.
+                if b['dir'] == DOWNLEFT:
+                    b['dir'] = DOWNRIGHT
+                if b['dir'] == UPLEFT:
+                    b['dir'] == UPRIGHT
+            if b['rect'].right > WINDOWWIDTH:
+                #The BOX has moved past the right.
+                if b['dir'] == DOWNRIGHT:
+                    b['dir'] = DOWNLEFT
+                if b['dir'] == UPRIGHT:
+                    b['dir'] = UPLEFT
 
+                    
