3a8a51c476995395ae5a89bad0ff0bda08d744da
SophiaRenats <90347782+SophiaRenats@users.noreply.github.com>
2022-01-13 10:26:06 -0500

    v0.2
    Setup PyGame Window.

--- collision_detection.py
+++ collision_detection.py
@@ -1,4 +1,4 @@
-# PyGame Collision Detection Practice, Sophia Rentas, January 13, 2022, 10:18am, v0.1
+# PyGame Collision Detection Practice, Sophia Rentas, January 13, 2022, 10:24am, v0.2
 
 import pygame, sys, random
 from pygame.locals import
@@ -6,3 +6,9 @@ from pygame.locals import
 # Setup PyGame
 pygame.init()
 mainClock = pygame.time.Clock()
+
+# Setup the PyGame Window
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 400
+windowSurface = pygame.disply.set_model((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+pygame.display.set_caption ('Collision Detection 2022')
